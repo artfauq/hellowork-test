cae3a432894c63f915ae1ffd278ed0a42ab08106
Arthur Fauquenot <arthurfauquenot@gmail.com>
2024-05-14 15:25:33 +0200

    config: update lint config + add config files

---
+++ .eslintrc.js
@@ -0,0 +1,119 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ */
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    project: 'tsconfig.eslint.json',
+    sourceType: 'module',
+  },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.mjs', '.ts', '.tsx'],
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx'],
+      },
+      typescript: {
+        alwaysTryTypes: true,
+        project: 'tsconfig.eslint.json',
+      },
+    },
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier', 'promise', 'unused-imports'],
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'eslint:recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:promise/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // Standard rules
+    'class-methods-use-this': 'off',
+    'consistent-return': 'error',
+    'default-case': 'error',
+    'global-require': 'warn',
+    'lines-between-class-members': [
+      'error',
+      'always',
+      {
+        exceptAfterSingleLine: true,
+      },
+    ],
+    'max-classes-per-file': 'off',
+    'no-console': 'off',
+    'no-extra-semi': 'off',
+    'no-param-reassign': 'off',
+    'no-promise-executor-return': 'off',
+    'no-underscore-dangle': 'off',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'any', prev: '*', next: 'import' },
+      { blankLine: 'any', prev: 'case', next: ['case', 'default'] },
+      { blankLine: 'any', prev: 'if', next: 'if' },
+      { blankLine: 'any', prev: 'export', next: 'export' },
+      { blankLine: 'any', prev: 'expression', next: 'expression' },
+      { blankLine: 'always', prev: ['const', 'let'], next: '*' },
+      { blankLine: 'any', prev: ['const', 'let'], next: ['const', 'let'] },
+    ],
+    'prefer-regex-literals': 'off',
+
+    // eslint-plugin-import rules
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+        mjs: 'never',
+      },
+    ],
+    'import/no-cycle': 'warn',
+    'import/order': [
+      'error',
+      {
+        'alphabetize': {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+        'groups': ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always',
+        'pathGroups': [
+          {
+            pattern: '{next,next/*,react,react/*}',
+            group: 'builtin',
+            position: 'after',
+          },
+        ],
+        'warnOnUnassignedImports': false,
+        'pathGroupsExcludedImportTypes': ['builtin'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+
+    // eslint-plugin-unused-imports rules
+    'unused-imports/no-unused-imports': 'error',
+
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+  },
+}
---
+++ ABOUT.md
@@ -0,0 +1,15 @@
+# Test technique Site Carrière
+
+Le but de cet exercice est de construire une petite application fullstack en next.js. Cette application doit :
+
+- Récupérer les offres emploi sur la ville de Bordeaux à partir de l'API de Jobijoba
+- Proposer une page (stylisée avec un peu de CSS) qui présente ces offres et permet d’y postuler (via l’URL de redirection)
+- Proposer une pagination
+
+Précisions :
+
+- Interface : vous êtes libre quant à l'affichage des offres (blocs, liste...).
+- Packaging : il faut que nous soyons capables de tester le projet localement rapidement, mais vous êtes libre sur la manière de nous l'envoyer.
+
+Durée : l'idée est de ne pas dépasser 3-4 heures, même si votre application n'est pas complètement terminée.
+Documentation API en PJ.
